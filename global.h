e60086de26d2d10260907dd7ea257aa6c8ee2a33
KostantinRudenko <rudenkokostya20@gmail.com>
2025-07-31 15:48:36 +0300

    added global.h file

---
+++ global.h
@@ -0,0 +1,27 @@
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#pragma region ______________________________ Constants
+
+#define LED_ON  HIGH
+#define LED_OFF LOW
+
+// Распиновка светодиодов, сенсора движения и пины транзисторов для сирены и зажигателя
+
+const uint8_t LED_1_RED_PIN = 0;
+const uint8_t LED_2_RED_PIN = 1;
+const uint8_t LED_3_RED_PIN = 2;
+const uint8_t LED_4_RED_PIN = 3;
+const uint8_t LED_5_RED_PIN = 4;
+const uint8_t LED_6_GREEN_PIN = 5;
+const uint8_t LED_7_GREEN_PIN = 6;
+
+const uint8_t PIR_SENSOR_PIN = 9;
+
+const uint8_t SIREN_PIN = 7;
+const uint8_t IGNITER_PIN = 10;
+
+const uint8_t MODE_BTN = 20;
+const uint8_t TIMER_BTN = 21;
+
+#endif // !_GLOBAL_H_
